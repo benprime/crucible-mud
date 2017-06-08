ac234928a95958fb1ac51fd6c5fe52a02de78719
Nicholas Mack <nichmack@gmail.com>
2017-06-07 22:56:58 -0500

    Begin Equip command: Listed out item stats and created stencil for equip command

--- data/itemData.js
+++ data/itemData.js
@@ -1,9 +1,15 @@
+
 module.exports = {
   catalog: [
     {
       name: "shortsword",
       displayName: "short sword",
       desc: "an iron short sword",
+      equip: "mainHand",  //mainHand or offHand or bothHand or eitherHand
+      damage: "1d6 + 0",  //die roll + modifier
+      type: "slashing",  //piercing, slashing, bludgeoning
+      speed: 0,  //scale from -1(slowest) to 1(fastest)
+      bonus: "damage + 0" //stat + modifier
     },
   ]
 };

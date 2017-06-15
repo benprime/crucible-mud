3ce5032b8a80f1f2817637b1278150aaa808916e
Nicholas Mack <nichmack@gmail.com>
2017-06-14 23:16:27 -0500

    start fleshing out the equip command

--- commands/equip.js
+++ commands/equip.js
@@ -14,7 +14,27 @@ module.exports = {
 
   execute(socket, itemName, hand) {
     //check user.inventory for itemName
-      //if no match emit "itemName is not in your inventory" and return
+    const inventory = socket.user.inventory;
+
+    // autocomplete name
+    const itemNames = inventory.map(item => item.displayName);
+    const completedNames = global.AutocompleteName(socket, itemName, itemNames);
+    if (completedNames.length === 0) {
+      socket.emit('output', { message: 'You don\'t have that item in your inventory.' });
+      return;
+    } else if (completedNames.length > 1) {
+      // todo: possibly print out a list of the matches
+      socket.emit('output', { message: 'Not specific enough!' });
+      return;
+    }
+
+    const item = inventory.find(item => item.displayName === completedNames[0]);
+
+    //if no match emit "itemName is not in your inventory" and return
+    if (item.equip === 'non') {
+      socket.emit('output', { message: 'You cannot equip that!' });
+      return;
+    }
       //if match add itemName to appropriate character item slot
       //add bonuses from itemName to corresponding character stats
   },
--- data/itemData.js
+++ data/itemData.js
@@ -5,7 +5,7 @@ module.exports = {
       name: "shortsword",
       displayName: "short sword",
       desc: "an iron short sword",
-      equip: "mainHand",  //mainHand or offHand or bothHand or eitherHand
+      equip: "mainHand",  //mainHand/offHand/bothHand/eitherHand, head, non, etc
       damage: "1d6 + 0",  //die roll + modifier
       type: "slashing",  //piercing, slashing, bludgeoning
       speed: 0,  //scale from -1(slowest) to 1(fastest)

ac8bedad74e41fec834924b2a4bfc0b3580fc8c2
Nicholas Mack <nichmack@gmail.com>
2017-09-07 00:12:44 -0500

    Started gossip test.  Also started looking into applying user stats to attack

--- data/itemData.js
+++ data/itemData.js
@@ -6,8 +6,9 @@ module.exports = {
       displayName: 'short sword',
       desc: 'an iron short sword',
       type: 'item',
+      range: 'melee',
       equip: 'mainHand',  //mainHand/offHand/bothHand/eitherHand, head, etc
-      damage: '1d6 + 0',  //die roll + modifier
+      damage: '1d6 0',  //die roll + modifier
       damageType: 'slashing',  //piercing, slashing, bludgeoning
       speed: 0,  //scale from -1(slowest) to 1(fastest)
       bonus: 'damage + 0', //stat + modifier
@@ -23,8 +24,9 @@ module.exports = {
       displayName: 'torch',
       desc: 'a flaming stick',
       type: 'item',
+      range: 'melee',
       equip: 'offHand',  //mainHand/offHand/bothHand/eitherHand, head, non, etc
-      damage: '1d2 + 0',  //die roll + modifier
+      damage: '1d2 0',  //die roll (+/-)modifier
       damageType: 'bludgeoning',  //piercing, slashing, bludgeoning
       speed: -.5,  //scale from -1(slowest) to 1(fastest)
       bonus: 'damage + 1d4 fire', //stat + modifier

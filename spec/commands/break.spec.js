bcd673496383509effb6f0ecb7542c131331050d
Ben Smith <bsmithpct@gmail.com>
2017-09-13 21:43:27 -0700

    Added tests for break command.

---
+++ spec/commands/break.spec.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const mocks = require('../mocks');
+const sut = require('../../commands/break');
+
+describe('break', function () {
+  let socket;
+
+  describe('execute', function () {
+
+    it('should set appropriate socket state variables when combat underway', function () {
+      // arrange
+      socket = new mocks.SocketMock();
+      socket.user = {
+        roomId: 123,
+        username: 'TestUser',
+        attackInterval: 1,
+        lastAttack: 1,
+        attackTarget: 1
+      };
+
+      // act
+      sut.execute(socket);
+
+      // assert
+      expect(socket.user.attackInterval).toBeUndefined();
+      expect(socket.user.lastAttack).toBeUndefined();
+      expect(socket.user.attackTarget).toBeUndefined();
+      expect(socket.broadcast.to).toHaveBeenCalledWith(123);
+      expect(socket.broadcast.to().emit).toHaveBeenCalledWith('output', { message: 'TestUser breaks off his attack.' });
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: '<span class="olive">*** Combat Disengaged ***</span>' });
+    });
+
+    it('should not emit messages if combat is not underway', function () {
+      // arrange
+      socket = new mocks.SocketMock();
+      socket.user = {
+        roomId: 123,
+        username: 'TestUser',
+        attackInterval: undefined,
+        lastAttack: undefined,
+        attackTarget: undefined
+      };
+
+      // act
+      sut.execute(socket);
+
+      // assert
+      expect(socket.user.attackInterval).toBeUndefined();
+      expect(socket.user.lastAttack).toBeUndefined();
+      expect(socket.user.attackTarget).toBeUndefined();
+      expect(socket.broadcast.to).not.toHaveBeenCalled();
+      expect(socket.broadcast.to().emit).not.toHaveBeenCalled();
+      expect(socket.emit).not.toHaveBeenCalledWith();
+    });
+  });
+
+});

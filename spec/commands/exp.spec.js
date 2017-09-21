3c79fbe6bbda06951f6ecc5f5f6b93023a013129
Ben Smith <bsmithpct@gmail.com>
2017-09-20 22:32:21 -0700

    Added test for exp command.

--- spec/commands/exp.spec.js
+++ spec/commands/exp.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mocks = require('../mocks');
+const User = require('../../models/user');
 const sut = require('../../commands/exp');
 
 describe('exp', function () {
@@ -9,11 +10,14 @@ describe('exp', function () {
 
   beforeAll(function() {
     socket = new mocks.SocketMock();
+    socket.user = new User();
     room = mocks.getMockRoom();
   });
 
   describe('execute', function () {
     it('should display the current experience level, xp, and next level', function() {
+      sut.execute(socket);
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: '<span class=\'cyan\'>XP: </span><span class=\'silver\'>undefined</span>\n<span class=\'cyan\'>Level: </span><span class=\'silver\'>undefined</span>\n<span class=\'cyan\'>Next: </span><span class=\'silver\'>NaN</span>\n' });
     });
   });
 

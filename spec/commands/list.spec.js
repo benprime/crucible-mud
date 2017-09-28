9257ba71413857c71d5dd47e4994d278d135e09a
Ben Smith <bsmithpct@gmail.com>
2017-09-27 22:44:39 -0700

    Added list command tests.

--- spec/commands/list.spec.js
+++ spec/commands/list.spec.js
@@ -5,34 +5,86 @@ const sut = require('../../commands/list');
 
 describe('list', function () {
   let socket;
+  let itemCatalog;
+  let mobCatalog;
 
-  beforeAll(function() {
+  beforeAll(function () {
     socket = new mocks.SocketMock();
+    itemCatalog = {
+      catalog: [
+        {
+          type: 'key',
+          displayName: 'key 1 display name',
+          name: 'key 1',
+        },
+        {
+          type: 'key',
+          displayName: 'key 2 display name',
+          name: 'key 2',
+        },
+        {
+          type: 'item',
+          displayName: 'item 1 display name',
+          name: 'item 1',
+        },
+        {
+          type: 'item',
+          displayName: 'item 2 display name',
+          name: 'item 2',
+        },
+      ],
+    };
+    mobCatalog = {
+      catalog: [
+        {
+          type: 'mob',
+          displayName: 'mob 1 display name',
+          name: 'mob 1',
+        },
+      ],
+    };
   });
 
   describe('execute', function () {
-    describe('when type is mobs', function() {
-      it('should lists mobs', function() {
-      });
+    it('should lists mob catalog', function () {
+      // arrange
+      const expectedString = '<table><tr><th>Name</th><th>Display Name</th></tr><tr><td>mob 1</td><td>mob 1 display name</td></tr></table>';
+
+      // act
+      sut.execute(socket, mobCatalog, 'mob');
+
+      // assert
+      expect(socket.emit.calls.mostRecent().args[0]).toBe('output');
+      expect(socket.emit.calls.mostRecent().args[1].message.includes(expectedString)).toBeTruthy(`message: ${socket.emit.calls.mostRecent().args[1].message} did not contain: ${expectedString}`);
     });
 
-    describe('when type is items', function() {
-      it('should lists items', function() {
-      });
+    it('should lists item catalog', function () {
+      // arrange
+      const expectedString = '<table><tr><th>Name</th><th>Display Name</th></tr><tr><td>item 1</td><td>item 1 display name</td></tr>\n<tr><td>item 2</td><td>item 2 display name</td></tr></table>';
+
+      // act
+      sut.execute(socket, itemCatalog, 'item');
+
+      // assert
+      expect(socket.emit.calls.mostRecent().args[0]).toBe('output');
+      expect(socket.emit.calls.mostRecent().args[1].message.includes(expectedString)).toBeTruthy(`message: ${socket.emit.calls.mostRecent().args[1].message} did not contain: ${expectedString}`);
     });
 
-    describe('when type is keys', function() {
-      it('should lists keys', function() {
-      });
+    it('should list key catalog', function () {
+      // arrange
+      const expectedString = '<table><tr><th>Name</th><th>Display Name</th></tr><tr><td>key 1</td><td>key 1 display name</td></tr>\n<tr><td>key 2</td><td>key 2 display name</td></tr></table>';
+
+      // act
+      sut.execute(socket, itemCatalog, 'key');
+
+      // assert
+      expect(socket.emit.calls.mostRecent().args[0]).toBe('output');
+      expect(socket.emit.calls.mostRecent().args[1].message.includes(expectedString)).toBeTruthy(`message: ${socket.emit.calls.mostRecent().args[1].message} did not contain: ${expectedString}`);
     });
 
   });
 
-  it('should output message when type is invalid', function() {
-  });
-
-  it('should be an admin command', function() {
+  it('should be an admin command', function () {
     expect(sut.admin).toBe(true);
   });
-
 });

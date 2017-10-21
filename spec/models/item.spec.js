0b7a856730cc5f7c65c4e9b18c8ab109b29a4f00
Ben Smith <bsmithpct@gmail.com>
2017-10-21 08:34:16 -0700

    Added item model tests.

--- spec/models/item.spec.js
+++ spec/models/item.spec.js
@@ -2,33 +2,34 @@
 
 const mocks = require('../mocks');
 const Item = require('../../models/item');
-const ObjectId = require('mongodb').ObjectId;
 
 describe('item model', function () {
+  let item;
+  let socket;
+
+  beforeAll(function () {
+    socket = new mocks.SocketMock();
+    item = new Item({
+      desc: 'Item Description',
+    });
+  });
+
+  beforeEach(function () {
+    socket.reset();
+  });
 
   describe('look', function () {
-    it('should build output string with just title and exits when short parameter is passed', function () {
+    it('should display item description', function () {
+      item.look(socket);
+
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: 'Item Description' });
     });
 
-    it('should include inventory in output when inventory length is not zero', function () {
-    });
+    it('should display item id if user is admin', function () {
+      socket.user.admin = true;
+      item.look(socket);
 
-    it('should omit inventory when user is carrying nothing', function () {
-    });
-
-    it('should include users in room when the user is not the only user in room', function () {
-    });
-
-    it('should omit users in room when the user is the only user in room', function () {
-    });
-
-    it('should include exits when there is at least one exit in the room', function () {
-    });
-
-    it('should disply none when there are no exits', function () {
-    });
-
-    it('should disply room id when user is an admin', function () {
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: 'Item Description\nItem ID: ' + item.id });
     });
   });
 });

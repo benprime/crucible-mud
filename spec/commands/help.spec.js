bee645c7b000793c11844ed2c863ed5ad8ba6bbf
Nicholas Mack <nichmack@gmail.com>
2017-10-25 23:55:35 -0500

    Help command unit tests for parameters

--- spec/commands/help.spec.js
+++ spec/commands/help.spec.js
@@ -5,20 +5,46 @@ const sut = require('../../commands/help');
 
 describe('help', function () {
   let socket;
+  let command;
 
-  beforeAll(function() {
+  beforeAll(function () {
     socket = new mocks.SocketMock();
+
+    command = {
+      name: 'gossip',
+      help: jasmine.createSpy('gossip')
+    }
+
+    sut.registerCommand(command);
   });
 
   describe('execute', function () {
-    it('should display general help with no parameters', function() {
 
-    });
-    it('should display topic help with a parameter', function() {
 
+    //TODO: Mock command handler
+
+
+
+    xit('should display general help with no parameters', function () {
+      sut.execute(socket);
+
+      //check general help output
+      //TODO: Preload entire help message into a variable or whatnot to check for accuracy
+      //expect(socket.emit).toHaveBeenCalledWith('output', { message: '' });
     });
-    it('should display error message when topic is invalid', function() {
-      
+
+    it('should display topic help with a parameter', function () {
+      sut.execute(socket,"gossip");
+
+      //check accuracy of output for gossip
+      expect(command.help).toHaveBeenCalled();
+    });
+
+    it('should display error message when topic is invalid', function () {
+      sut.execute(socket,"yourface");
+
+      //check output for bad command
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: 'No help for that topic.' });
     });
   });
 

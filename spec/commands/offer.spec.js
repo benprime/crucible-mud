ffe03608fba12670fe938cdf9534ef81bb15b484
mrjeffms <jeff@jeffmsmith.com>
2017-11-15 22:37:22 -0800

    Updated the offer and take commands to store the offer on the target users socket.

--- spec/commands/offer.spec.js
+++ spec/commands/offer.spec.js
@@ -2,6 +2,8 @@
 
 const mocks = require('../mocks');
 const sut = require('../../commands/offer');
+const autocomplete = require('../../autocomplete');
+const room = require('../../models/room');
 
 describe('offer', function () {
   let socket;
@@ -10,9 +12,36 @@ describe('offer', function () {
     socket = new mocks.SocketMock();
   });
 
+  describe('dispatch', function () {
+    beforeEach(function () {
+      spyOn(sut, 'execute');
+    });
+
+    it('should call execute with match', function () {
+      sut.dispatch(socket, ['offer', 'aUser', 'aItem']);
+      expect(sut.execute).toHaveBeenCalledWith(socket, 'aUser', 'aItem');
+    });
+  });
+
   describe('execute', function () {
-    it('should', function() {
-      //TODO: when offer method is re-implemented
+    let autocompleteResult;
+
+    beforeEach(function(){
+      spyOn(autocomplete, 'autocomplete').and.callFake(() => autocompleteResult);
+    });
+
+    it('should output message when item is not in inventory', function() {
+      autocompleteResult = [];
+
+      sut.execute(socket, 'aUser', 'aItem');
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: `aItem is not in your inventory!` });
+    });
+
+    it('should output message when item is not in inventory', function() {
+      autocompleteResult = [];
+
+      sut.execute(socket, 'aUser', 'aItem');
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: `Many items can be described as aItem. Be more specific.` });
     });
 
   });

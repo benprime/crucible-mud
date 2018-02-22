400a68d23d240b25720b3d5bc0e5b5a2bed70fbb
mrjeffms <jeff@jeffmsmith.com>
2018-02-21 22:33:30 -0800

    Added first couple of tests for take command

--- spec/commands/take.spec.js
+++ spec/commands/take.spec.js
@@ -2,6 +2,8 @@
 
 const mocks = require('../mocks');
 const sut = require('../../commands/take');
+const autocomplete = require('../../autocomplete');
+const Room = require('../../models/room');
 
 describe('take', function () {
   let socket;
@@ -10,14 +12,52 @@ describe('take', function () {
     socket = new mocks.SocketMock();
   });
 
+  describe('dispatch', function(){
+    beforeEach(function(){
+      spyOn(sut, 'execute');
+      socket.emit.calls.reset();
+    });
+
+    it('should call execute with match', function(){
+      sut.dispatch(socket, ['take', 'aItem']);
+      expect(sut.execute).toHaveBeenCalledWith(socket, 'aItem');
+    });
+
+    it('should output message if multiple matches', function(){
+      sut.dispatch(socket, 'take', 'aItem', 'anotherItem');
+      expect(socket.emit).toHaveBeenCalledWith('output', { message: 'What do you want to take?' });
+    });
+  });
+
+
   describe('execute', function () {
-    it('should output message when item is not found', function() {
+    let otherUserSocket = new mocks.SocketMock();
+    beforeEach(function(){
+      spyOn(global, 'GetSocketByUsername').and.returnValue(otherUserSocket);
+    })
+
+    it('should add offered item to inventory', function(){
+      let offeredItem = {id: 'aItemId', name:'aItem'}
+      otherUserSocket.user.inventory = [offeredItem];
+      socket.offers = [{ 
+        fromUserName: 'aUser',
+        toUserName: 'TestUser',
+        item: offeredItem
+      }]
+
+      sut.execute(socket, 'aItem');
+
+      expect(socket.user.inventory[0].name).toEqual('aItem');
+      expect(otherUserSocket.user.inventory.length).toEqual(0);
     });
-    it('should output message when item is fixed', function() {
+
+    xit('should output message when item is not found', function() {
     });
-    it('should update the room/user and save room/user to database', function() {
+    xit('should output message when item is fixed', function() {
     });
-    it('should output message when command successful', function() {
+    xit('should update the room/user and save room/user to database', function() {
+    });
+    xit('should output message when command successful', function() {
     });
   });
 });

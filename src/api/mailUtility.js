b755d1e2540ccd29b4db17976081070fd504ed9f
Ben Smith <bsmithpct@gmail.com>
2022-04-24 12:33:03 -0700

    Re-setting up mailgun

--- src/api/mailUtility.js
+++ src/api/mailUtility.js
@@ -1,12 +1,30 @@
 import nodemailer from 'nodemailer';
+import mailgun from 'mailgun-js';
 
 let transporter;
+const devMode = (!process.env.NODE_ENV || process.env.NODE_ENV.trim() === 'development');
 
-function setup() {
-  transporter = nodemailer.createTransport({
-    host: 'localhost',
-    port: 25
-  });
+const mailGunApiKey = process.env.MAIL_GUN_API_KEY;
+const mailGunDomain = process.env.MAIL_GUN_DOMAIN;
+let mg;
+
+async function setup() {
+
+  if (devMode) {
+    let testAccount = await nodemailer.createTestAccount();
+
+    transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+  } else {
+    mg = mailgun({ apiKey: mailGunApiKey, domain: mailGunDomain });
+  }
 }
 
 /**
@@ -21,33 +39,33 @@ function setup() {
  * })
  */
 exports.sendMail = async function (email) {
+  if (devMode) {
+    try {
+      const result = await transporter.sendMail({
+        from: email.from,
+        to: email.to,
+        subject: email.subject,
+        text: email.text,
+        html: email.html,
+      });
 
-  try
-  {
-    const result = await transporter.sendMail({
-      from: email.from,
-      to: email.to,
-      subject: email.subject,
-      text: email.text,
-      html: email.html,
-    });
-    
-    console.log('Message sent: %s', result.messageId, '\n');
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(result), '\n');
-  }
-  catch(error)
-  {
+      console.log('Message sent: %s', result.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(result));
+    }
+    catch(error) {
       console.error('Could not send mail! ' + error);
+    }
+  } else {
+    mg.messages().send(email, function (error, body) {
+      console.log(body, error);
+    });
   }
 };
 
 // create transporter
-try
-{
+try {
   setup();
 }
-catch(error)
-{
+catch(error) {
   console.error('Could not connect to mail server! ' + error);
 }
-
